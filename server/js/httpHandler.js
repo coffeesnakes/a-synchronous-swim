8d07b92b7cb8087234d699d92fc4a0ee5c5ac447
Jacob Berg <coffeesnakes@gmail.com>
2020-12-12 15:26:33 -0800

    user keyboard inputs are responsive with client/server relationship working

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -1,30 +1,52 @@
-const fs = require('fs');
-const path = require('path');
-const headers = require('./cors');
-const multipart = require('./multipartUtils');
+const fs = require("fs");
+const path = require("path");
+const headers = require("./cors");
+const multipart = require("./multipartUtils");
 
-// Path for the background image ///////////////////////
-module.exports.backgroundImageFile = path.join('.', 'background.jpg');
-////////////////////////////////////////////////////////
+module.exports.backgroundImageFile = path.join(".", "background.jpg");
 
-let messageQueue = null;
+let messageQueue = require("./messageQueue");
+httpHandler.initialize(queue);
 module.exports.initialize = (queue) => {
   messageQueue = queue;
 };
 
 module.exports.router = (req, res, next = () => {}) => {
-  console.log("Serving request type " + req.method + " for url " + req.url);
-  res.writeHead(200, headers);
-  res.end();
-  next();
-
-  // invoke next() at the end of a request to help with testing!
-  if (req.method === "GET" && req.url === "/") {
+  if (req.methods === "OPTIONS") {
     res.writeHead(200, headers);
+    res.end();
     next();
   }
+
+  if (req.method === "GET") {
+    if (req.path === "/") {
+      res.writeHead(200, headers);
+      res.end(messageQueue.dequeue());
+    } else if (req.url === "/background.jpg") {
+      fs.readFile(module.exports.backgroundImageFile, (err, data) => {
+        if (err) {
+          res.writeHead(404, headers);
+        } else {
+          res.writeHead(200, headers);
+          // res.end(data)
+          res.write(data, "binary");
+        }
+        res.end();
+        next();
+      });
+      res.writeHead(404, headers);
+      res.end();
+    }
+  }
+  if (req.method === "POST" && req.url === "/background.jpg") {
+    let data = fs.writeFile(module.exports.backgroundImageFile, data, (err) => {
+      if (err) {
+        res.writeHead(404, headers);
+      } else {
+        res.writeHead(201, headers);
+      }
+      res.end();
+      next();
+    });
+  }
 };
-
-
-
-
--- server/spec/httpHandler.spec.js
+++ server/spec/httpHandler.spec.js
@@ -6,7 +6,7 @@ const server = require('./mockServer');
 
 const httpHandler = require('../js/httpHandler');
 
-
+httpHandler.initialize(queue);
 
 describe('server responses', () => {
 
@@ -24,16 +24,18 @@ describe('server responses', () => {
   it('should respond to a GET request for a swim command', (done) => {
     // write your test here
     let {req, res} = server.mock('/', 'GET');
+    queue.enqueue('up')
     httpHandler.router(req, res);
     expect(res._responseCode).to.equal(200);
     expect(res._ended).to.equal(true);
     expect(['left','right','up','down']).to.include(res._data.toString());
+
     done();
   });
 
-  xit('should respond with 404 to a GET request for a missing background image', (done) => {
+  xit('should respond with 404 to a GET request for a missing   image', (done) => {
     httpHandler.backgroundImageFile = path.join('.', 'spec', 'missing.jpg');
-    let {req, res} = server.mock('FILL_ME_IN', 'GET');
+    let {req, res} = server.mock('/background.jpg', 'GET');
 
     httpHandler.router(req, res, () => {
       expect(res._responseCode).to.equal(404);
